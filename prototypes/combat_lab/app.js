70d78238316104fd7c4afa048aa1fb6ab158da7f
almercapa <almercapallasantos@gmail.com>
2026-01-10 21:13:27 -0500

    Change log: Added player HP bar with dynamic timer that shortens HP once adaptation is complete UI changes like HP bar distinctions, names above HP bars, timer before adaptation, more concise result message

--- prototypes/combat_lab/app.js
+++ prototypes/combat_lab/app.js
@@ -5,27 +5,36 @@ let maho_bar = document.getElementById("maho-fill");
 let play_hp = 100;
 let play_bar = document.getElementById("player-fill");
 let rt = document.querySelector(".result");
+let timer = document.getElementById("timer-message");
+let sec = 5;
 let adapt;
 
 function startTimer() {
-    adapt = setInterval(reduceHP, 5000);
+    adapt = setInterval(reduceHP, 1000);
 }
 
 function reduceHP() {
-    if (play_hp > 0) {
+    sec -= 1;
+    timer.innerText = "Adaptation in " + sec + " seconds";
+    if (play_hp > 0 && sec === 0) {
         play_hp -= 12.5;
         play_bar.style.width = `${play_hp}%`;
+        sec = 5;
+        timer.innerText = "Adaptation in "  + sec + " seconds";
     }
     if (play_hp <= 0 && maho_hp <= 0) {
         clearInterval(adapt);
+        timer.innerText = "";
         rt.style.color = "maroon";
         rt.innerText = "Mutual Destruction - Mahoraga Adapted First";
     } else if (play_hp <= 0) {
         clearInterval(adapt);
+        timer.innerText = "";
         rt.style.color = "maroon";
         rt.innerText = "You have been defeated";
     } else if (maho_hp <= 0) {
         clearInterval(adapt);
+        timer.innerText = "";
         rt.style.color = "forestgreen";
         rt.innerText = "Mahoraga has been defeated";
     }
@@ -38,14 +47,17 @@ atk.addEventListener("click", (e) => {
     }
     if (play_hp <= 0 && maho_hp <= 0) {
         clearInterval(adapt);
+        timer.innerText = "";
         rt.style.color = "maroon";
         rt.innerText = "Mutual Destruction - Mahoraga Adapted First";
     } else if (play_hp <= 0) {
         clearInterval(adapt);
+        timer.innerText = "";
         rt.style.color = "maroon";
         rt.innerText = "You have been defeated";
     } else if (maho_hp <= 0) {
         clearInterval(adapt);
+        timer.innerText = "";
         rt.style.color = "forestgreen";
         rt.innerText = "Mahoraga has been defeated";
     }
@@ -58,6 +70,8 @@ rst.addEventListener("click", (e) =>  {
     maho_bar.style.width = `100%`;
     play_hp = 100;
     play_bar.style.width = `100%`;
+    sec = 5;
+    timer.innerText = "Adaptation in " + sec + " seconds";
     startTimer();
 });
 
